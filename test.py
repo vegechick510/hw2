0c2bb754b61b8df7e1827659c805648f0a4bf9d4
Xiang Lan <34869470+xianglan010@users.noreply.github.com>
2024-09-06 21:11:25 +0000

    add test case 1

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
 from hw2_debugging import merge_sort
 
-arr_1 = [4,-3,2,5,4,0,1,1,3]
-
-arr_1_sorted = [-3,0,1,1,2,3,4,4,5]
-
-merge_out_1 = merge_sort(arr_1)
-
-assert merge_out_1 == arr_1_sorted
+def test_case_1():
+    arr_1 = [4,-3,2,5,4,0,1,1,3]
+    arr_1_sorted = [-3,0,1,1,2,3,4,4,5]
+    merge_out_1 = merge_sort(arr_1)
+    assert merge_out_1 == arr_1_sorted
