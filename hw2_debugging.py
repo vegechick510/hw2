39373b24364a6e6622fe814e4e250859674393a7
Xiang Lan <34869470+xianglan010@users.noreply.github.com>
2024-09-06 02:58:00 +0000

    correct all spelling to avoid pylint warning

--- hw2_debugging.py
+++ hw2_debugging.py
@@ -1,40 +1,46 @@
 import rand
 
 
-def mergeSort(arr):
-
+def merge_sort(arr):
+    """
+    Sorts  array.
+    """
     if len(arr) == 1:
         return arr
 
     half = len(arr)//2
 
-    return recombine(mergeSort(arr[:half]), mergeSort(arr[half:]))
+    return recombine(merge_sort(arr[:half]), merge_sort(arr[half:]))
 
 
-def recombine(leftArr, rightArr):
-    leftIndex = 0
-    rightIndex = 0
-    mergeArr = [None] * (len(leftArr) + len(rightArr))
-    while leftIndex < len(leftArr) and rightIndex < len(rightArr):
-        if leftArr[leftIndex] < rightArr[rightIndex]:
-            # print(leftIndex,rightIndex)
-            mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
-            leftIndex += 1
+def recombine(left_arr, right_arr):
+    """
+    Merges two sorted arrays into one array.
+
+    """
+    left_index = 0
+    right_index = 0
+    merge_arr = [None] * (len(left_arr) + len(right_arr))
+    while left_index < len(left_arr) and right_index < len(right_arr):
+        if left_arr[left_index] < right_arr[right_index]:
+            # print(left_index,right_index)
+            merge_arr[left_index + right_index] = left_arr[left_index]
+            left_index += 1
         else:
             # print(leftIndex,rightIndex)
-            mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
-            rightIndex += 1
+            merge_arr[left_index + right_index] = right_arr[right_index]
+            right_index += 1
 
-    for i in range(rightIndex, len(rightArr)):
-        mergeArr[leftIndex + i] = rightArr[i]
+    for i in range(right_index, len(right_arr)):
+        merge_arr[left_index + i] = right_arr[i]
 
-    for i in range(leftIndex, len(leftArr)):
-        mergeArr[rightIndex + i] = leftArr[i]
+    for i in range(left_index, len(left_arr)):
+        merge_arr[right_index + i] = left_arr[i]
 
-    return mergeArr
+    return merge_arr
 
 
 arr1 = rand.random_array([None] * 20)
-arr_out = mergeSort(arr1)
+arr_out = merge_sort(arr1)
 
 print(arr_out)
