72a96c03714904ee14ae501f6ae78498cfbaa68a
Xiang Lan <34869470+xianglan010@users.noreply.github.com>
2024-09-10 20:59:20 +0000

    add traces for pre and post

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
 from hw2_debugging import merge_sort
 
 def test_case_1():
-    arr_1 = [4,-3,2,5,4,0,1,1,3]
-    arr_1_sorted = [-3,0,1,1,2,3,4,4,5]
-    merge_out_1 = merge_sort(arr_1)
-    assert merge_out_1 == arr_1_sorted
+    arr_in = [4,-3,2,5,4,0,1,1,3]
+    arr_sorted = [-3,0,1,1,2,3,4,4,5]
+    arr_out = merge_sort(arr_in)
+    assert arr_out == arr_sorted
 
 def test_case_2():
-    arr_1 = [0,0,0,0,1,0,0,0,0]
-    arr_1_sorted = [0,0,0,0,0,0,0,0,1]
-    merge_out_1 = merge_sort(arr_1)
-    assert merge_out_1 == arr_1_sorted
+    arr_in = [0,0,0,0,1,0,0,0,0]
+    arr_sorted = [0,0,0,0,0,0,0,0,1]
+    arr_out = merge_sort(arr_in)
+    assert arr_out == arr_sorted
 
 def test_case_3():
     arr_in = [5, 5, 1, 6, -1, 4, 3, 7, 5]
