50f88d5bc1b302e40a635c18d4c896693367427f
Amber Shek <mibybymi@163.com>
2024-09-10 01:01:51 -0400

    Update test.py

--- test.py
+++ test.py
@@ -11,3 +11,9 @@ def test_case_2():
     arr_1_sorted = [0,0,0,0,0,0,0,0,1]
     merge_out_1 = merge_sort(arr_1)
     assert merge_out_1 == arr_1_sorted
+
+def test_case_3():
+    arr_in = [5, 5, 1, 6, -1, 4, 3, 7, 5]
+    arr_sorted = [-1, 1, 3, 4, 5, 5, 5, 6, 7]
+    arr_out = merge_sort(arr_in)
+    assert arr_out == arr_sorted
